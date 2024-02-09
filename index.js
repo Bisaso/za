6461c7da76e24e4607067977f463e7a9f24c5e2d
Bisaso <159421343+Bisaso@users.noreply.github.com>
2024-02-09 07:23:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function assignment(email,password){
+const validEmail = "bisasoabubakar@gmail.com"
+const validPassword ="21/U/04269/EVE@2024"
+
+if(email =validEmail , password = validPassword){
+    console.log('user email is{email},login successfully%');
+} else 
+{
+    console.log ('incorrect user credentials');
+
+}
+
+}
+assignment('bisasoabubakar@gmail.com','21/U/04269/EVE@2024')
